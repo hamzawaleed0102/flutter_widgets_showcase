71f6a4061d36355bfd00efae54279bb5d2d6fb1c
hamzawaleed0102 <hamzawaleed0102@gmail.com>
2020-04-02 14:08:40 +0500

    Update sidedrawer

--- lib/main.dart
+++ lib/main.dart
@@ -17,40 +17,55 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer:Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.redAccent),
-              child: Text("asdf"),
+      drawer: Drawer(
+          child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.redAccent),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Flutter Widgets",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                Text("Showcase",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300)),
+              ],
             ),
-            ListTile(
-              title: Text("Buttons"),
-              onTap: (){
-//
-              },
-            )
-
-          ],
-        )
-      ),
+          ),
+          ListTile(
+            title: Row(
+              children: <Widget>[
+                Icon(Icons.touch_app),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12),
+                  child: Text("Buttons"),
+                )
+              ],
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      )),
       appBar: AppBar(
-        title: Text('widget.title'),
+        title: Text('Flutter Showcase'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'asdf',
-              style: Theme.of(context).textTheme.display1,
+              'Home Page',
             ),
           ],
         ),
